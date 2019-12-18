31ee2ce6fbf008b41bb8fb2bbeef9043aada7746
GatesKennedy <conor@esccollective.com>
2019-12-17 21:01:52 -0800

    MGMT: .../scripts/bld_tables_all.sql , rename tbl_favorites -> tbl_history

--- data/scripts/bld_tables_All.sql
+++ data/scripts/bld_tables_All.sql
@@ -3,15 +3,15 @@ USE db_nds;
 CREATE TABLE tbl_library(
     id SERIAL PRIMARY KEY,
     data_json jsonb NOT NULL DEFAULT '{"nuns":"bums"}'::jsonb,
-    song_url VARCHAR(200) NOT NULL DEFAULT 'NO_URL'
+    song_url VARCHAR NOT NULL DEFAULT 'NO_URL'
 );
 
 
 CREATE TABLE tbl_user(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR(100) NOT NULL,
     date_join date NOT NULL
 );
 
@@ -19,15 +19,15 @@ CREATE TABLE tbl_user(
 CREATE TYPE entity AS ENUM ('personal', 'biz_sml', 'biz_med', 'biz_lrg');
 CREATE TABLE tbl_profile(
     user_id INTEGER REFERENCES tbl_user(id),
-    entity_type entity NOT NULL,
+    entity_type entity NOT NULL DEFAULT 'personal',
     payment_info VARCHAR NOT NULL
 );
 
-CREATE TABLE tbl_favorite(
+CREATE TABLE tbl_history(
     user_id INTEGER REFERENCES tbl_user(id),
     library_id INTEGER REFERENCES tbl_library(id),
     playlist_id INTEGER REFERENCES tbl_playlist(id),
-    payment_status BOOL NOT NULL DEFAULT false,
+    payment_status BOOLEAN NOT NULL DEFAULT 'false',
     PRIMARY KEY (user_id, song_id)
 );
 
