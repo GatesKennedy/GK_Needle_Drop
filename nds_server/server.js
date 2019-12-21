cdc8719b4687861aa5c0d4179f72ddecc932563b
GatesKennedy <conor@esccollective.com>
2019-12-21 01:17:30 -0800

    $FLAG$ NEW: nds_client/ , init React App ,, MOD: package.json , update scripts to run concurrently , bypass WWW file ,, MOD: nds_server/server.js , consolidate fxn from WWW file

--- nds_server/server.js
+++ nds_server/server.js
@@ -3,6 +3,9 @@ const pool = require('../nds_db/db');
 const browse = require('./nds_fileMgmt/api/browse')
 
 const serv = express();
+const PORT = process.env.PORT || 5000;
+
+serv.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 serv.use('/browse', browse);
 
