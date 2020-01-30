96afeb731298261a146aa8388a8180bb9387dc6c
GatesKennedy <conor@esccollective.com>
2020-01-29 18:12:36 -0800

    NEW: fldr for General Components to be used cross-app

---
+++ nds_client/src/components/General/FxnExpand.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FxnExpand = ({ head, body }) => {
+  return <div></div>;
+};
+
+FxnExpand.propTypes = {
+  head: PropTypes.object.isRequired,
+  body: PropTypes.object.isRequired
+};
+
+export default FxnExpand;
