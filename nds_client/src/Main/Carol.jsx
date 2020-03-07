f46dbc14dbdbfeed3c5e438d81c5c6ee5e91e838
GatesKennedy <conor@esccollective.com>
2020-03-07 06:24:04 -0800

    MOD: /Main/Carol.jsx , Convert demo to functional Component , map state to populate NDS_playlists" ;

--- nds_client/src/Main/Carol.jsx
+++ nds_client/src/Main/Carol.jsx
@@ -1,49 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment, useEffect } from 'react';
 import Slider from 'react-slick';
 
 import PropTypes from 'prop-types';
-
+//  REDUX
+import { connect } from 'react-redux';
+import {
+  getPListNames,
+  getPlayAll,
+  getPlaylist
+} from '../Modules/Library/rdx_axn/axn_playlist';
 import CarolMockasin from '../Modules/Library/assets/img/carol-mocks.png';
 
-export default class Carol extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    };
-    return (
-      <div>
-        <Slider {...settings}>
+const Carol = ({ getPListNames, playlist }) => {
+  useEffect(() => {
+    getPListNames();
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        {playlist.pListNames ? (
+          playlist.pListNames.map(plist => (
+            <div>
+              <img src={CarolMockasin} />
+              <h1>{plist.name}</h1>
+            </div>
+          ))
+        ) : (
           <div>
             <img src={CarolMockasin} />
+            <h1>demolishit</h1>
           </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-          <div>
-            <img src={CarolMockasin} />
-          </div>
-        </Slider>
-      </div>
-    );
-  }
-}
+        )}
+
+        <div>
+          <img src={CarolMockasin} />
+          <h1>demolishit</h1>
+        </div>
+        <div>
+          <img src={CarolMockasin} />
+          <h1>demolishit</h1>
+        </div>
+        <div>
+          <img src={CarolMockasin} />
+          <h1>demolishit</h1>
+        </div>
+        <div>
+          <img src={CarolMockasin} />
+          <h1>demolishit</h1>
+        </div>
+        <div>
+          <img src={CarolMockasin} />
+          <h1>demolishit</h1>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 Carol.propTypes = {};
 
-//export default Carol;
+const mapStateToProps = state => ({
+  playlist: state.playlist
+});
+
+export default connect(mapStateToProps, { getPListNames })(Carol);
